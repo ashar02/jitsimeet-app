113c27f341480c820045657bda8bae43732d70a0
MAshar <ashar@claryicon.com>
2021-08-27 23:23:32 +0500

    Initial android configuration.

--- App.js
+++ App.js
@@ -1,114 +1,147 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState, useEffect} from 'react';
 import {
-  SafeAreaView,
+  TextInput,
   StyleSheet,
-  ScrollView,
+  Button,
+  KeyboardAvoidingView,
   View,
   Text,
-  StatusBar,
+  Alert,
+  Switch,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import JitsiMeet, {JitsiMeetView} from 'react-native-jitsi-meet';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+function App() {
+  const [userName, setUserName] = useState('');
+  const [callLink, setCallLink] = useState('');
+  const [callStarted, setCallStarted] = useState(false);
+  const [isVideoEnabled, setIsVideoEnabled] = useState(false);
+
+  useEffect(() => {
+    return () => {
+      console.log('JitsiMeet end call');
+      JitsiMeet.endCall();
+    };
+  });
+
+  const toggleSwitch = () =>
+    setIsVideoEnabled((previousState) => !previousState);
+
+  const onConferenceTerminated = (nativeEvent) => {
+    /* Conference terminated event */
+    console.log('onConferenceTerminated: ', {nativeEvent});
+    setCallLink('');
+    setCallStarted(false);
+    setIsVideoEnabled(false);
+  };
+
+  const onConferenceJoined = (nativeEvent) => {
+    /* Conference joined event */
+    console.log('onConferenceJoined: ', {nativeEvent});
+  };
+
+  const onConferenceWillJoin = (nativeEvent) => {
+    /* Conference will join event */
+    console.log('onConferenceWillJoin:', {nativeEvent});
+  };
+
+  const startCall = () => {
+    if (userName !== '' && callLink !== '') {
+      setCallStarted(true);
+      setTimeout(() => {
+        const url = `https://video.circleit.com/${callLink}`;
+        const userInfo = {
+          displayName: userName,
+          email: 'user@example.com',
+          avatar: 'https:/gravatar.com/avatar/abc123',
+        };
+        if (isVideoEnabled) {
+          JitsiMeet.call(url, userInfo);
+        } else {
+          JitsiMeet.audioCall(url);
+        }
+      }, 1000);
+    } else {
+      Alert.alert('Make sure to enter your name and call-link!');
+    }
+  };
+
+  if (!callStarted) {
+    return (
+      <KeyboardAvoidingView style={styles.container}>
+        <Text style={styles.text}>Join a call</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setUserName}
+          value={userName}
+          placeholder="Enter your name"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={setCallLink}
+          value={callLink}
+          placeholder="Enter call link"
+        />
+        <View
+          style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+          <Switch
+            trackColor={{false: '#767577', true: '#81b0ff'}}
+            thumbColor={isVideoEnabled ? '#29d' : '#f4f3f4'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleSwitch}
+            value={isVideoEnabled}
+            style={{
+              alignSelf: 'flex-start',
+              marginLeft: 10,
+            }}
+          />
+          <Text style={styles.text}>Join with your video</Text>
+        </View>
+        <View style={styles.buttonWrapper}>
+          <Button title="Start Call" onPress={startCall} />
+        </View>
+      </KeyboardAvoidingView>
+    );
+  }
+  if (callStarted) {
+    return (
+      <JitsiMeetView
+        onConferenceTerminated={(e) => onConferenceTerminated(e)}
+        onConferenceJoined={(e) => onConferenceJoined(e)}
+        onConferenceWillJoin={(e) => onConferenceWillJoin(e)}
+        style={styles.callContainer}
+      />
+    );
+  }
+  return null;
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    height: '100%',
+    width: '100%',
+    justifyContent: 'center',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  callContainer: {
+    flex: 1,
+    height: '100%',
+    width: '100%',
   },
-  body: {
-    backgroundColor: Colors.white,
+  text: {textAlign: 'center'},
+  buttonWrapper: {
+    marginTop: 30,
+    minWidth: 200,
+    alignSelf: 'center',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  input: {
+    paddingHorizontal: 10,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 5,
   },
 });
-
 export default App;
